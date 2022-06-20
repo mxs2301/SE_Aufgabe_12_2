e1f926281477c81e613008a29c213361dd2d3037
Max Schopf <max.schopf@mailfence.com>
2022-06-20 12:28:12 +0200

    Testen von der Factoryerzeugung

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -3,5 +3,15 @@ package com.app;
 public class Main{
 	public static void main(String[] args) {
 		System.out.println("Help");
+		
+		AnimalBreeder Max = new AnimalBreeder();
+
+		Max.getAnimal("Tiger");
+
+		Animal party = Max.getAnimal("Tiger");
+
+		party.roar();
+
+
 	}
 }
